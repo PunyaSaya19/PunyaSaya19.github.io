33c547dc499aa338acd14037f615d430e690020d
punyasaya19 <rifanhdy91@gmail.com>
2022-11-14 12:39:59 +0700

    First commit new design

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const nav_toggle = document.querySelector("#my_nav_toggle");
+const nav_menu = document.querySelector("#my_nav_menu");
+
+nav_toggle.addEventListener('click', function() {
+    nav_menu.classList.toggle('active')
+});
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items : 1,
+        margin : 30,
+        loop : true,
+        responsive : {
+            576 : {
+                items : 2,
+            }
+        }
+    });
+  });
