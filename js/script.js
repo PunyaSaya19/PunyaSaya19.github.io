588e3c0e3159a496bb0bdc51c6f1d4df54f74e13
punyasaya19 <rifanhdy91@gmail.com>
2023-05-10 09:18:54 +0700

    halaman portfolio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,124 @@
 // alert(window.innerWidth)
+
+// variables
+// for portfolio
+const PortfolioContainer = document.querySelector('.portfolio-content-desc');
+const PortfolioDetail = document.querySelector('#portfolio-detail');
+const PortfolioCloseBtn = document.querySelector('#portfolio-detail-close');
+const PortfolioDetailImgDisplay = document.querySelector('.portfolio-detail-img-list');
+const PortfolioDetailImgDescription = document.querySelector('.portfolio-detail-content');
+const PortfolioBtnNext = document.querySelector('#portfolio-detail-img-nav-next');
+const PortfolioBtnPrev = document.querySelector('#portfolio-detail-img-nav-prev');
+const PortfolioLists = document.querySelectorAll('.portfolio-content-list');
+let PortfolioIDProject = null;
+let PortfolioImgNow = 0;
+let PortfolioImgMax = 0;
+let PortfolioImgSource = '';
+let PortfolioImgDesc = '';
+
+
+// functions
+// for portfolio
+function showPortfolioLayer(show = true) {
+    if(show) {
+        PortfolioDetail.classList.add('active');        
+    } else {
+        PortfolioDetail.classList.remove('active');
+    }
+}
+function filterPortfolioData(datas, id) {
+    let rslt = null;
+    datas.forEach((dt) => {
+        if(dt.id == id) {
+            rslt = dt;
+        }
+    });
+    return rslt;
+}
+function setPortfolioVariables(data) {
+    PortfolioIDProject = data.id;
+    PortfolioImgMax = data.img_count;
+    PortfolioImgSource = data.img_folder;
+    PortfolioImgDesc = data.deskripsi;
+    PortfolioImgNow = 0;
+}
+function showPortfolioImg(source, name) {
+    PortfolioDetailImgDisplay.setAttribute('src', source + name + '.png')
+}
+function changePortfolioDesc() {
+    PortfolioDetailImgDescription.innerHTML = PortfolioImgDesc;
+}
+function getDataPortfolio(id) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = (e) => {
+        if(xhr.status == 200) {
+            const result = JSON.parse(xhr.responseText);
+            const filterData = filterPortfolioData(result.datas, id);
+            setPortfolioVariables(filterData);
+            showPortfolioImg(PortfolioImgSource, 'main');
+            changePortfolioDesc();
+            showPortfolioLayer(true);
+        } else {
+            alert('Gagal load data');
+            showPortfolioLayer(false);
+        }
+    }
+    xhr.open('GET', 'resource.json');
+    xhr.send();
+}
+function loadAllPortfolio() {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = (e) => {
+        if(xhr.status == 200) {
+            const result = JSON.parse(xhr.responseText);
+            PortfolioContainer.innerHTML = loadPortfolioTemplate(result.datas);
+        } else {
+            alert('Gagal load data');
+        }
+    }
+    xhr.open('GET', 'resource.json');
+    xhr.send();
+}
+function loadPortfolioTemplate(data) {
+    let tmplt = ``;
+    data.forEach((dt) => {
+        tmplt += `
+        <div class="portfolio-content-list" data-id="${dt.id}" onclick="displayPortfolioDetail('${dt.id}')">
+            <img src="${dt.img_folder}main.png" alt="Portfolio Photo" srcset="" class="portfolio-content-list-img">
+            <h4 class="portfolio-content-list-title">
+                ${dt.judul}
+            </h4>
+            <div class="portfolio-content-list-layer">
+                <i class="fas fa-plus"></i>
+            </div>
+        </div>
+        `;
+    });
+    return tmplt;
+}
+function displayPortfolioDetail(id) {
+    getDataPortfolio(id);
+}
+// actions
+// for portfolio
+PortfolioCloseBtn.addEventListener('click', () => {
+    showPortfolioLayer(false);
+})
+PortfolioBtnNext.addEventListener('click', () => {
+    if(PortfolioImgNow < PortfolioImgMax) {
+        PortfolioImgNow = PortfolioImgNow + 1;
+        showPortfolioImg(PortfolioImgSource, PortfolioImgNow);
+    } 
+})
+PortfolioBtnPrev.addEventListener('click', () => {
+    if(PortfolioImgNow > 1) {
+        PortfolioImgNow = PortfolioImgNow - 1;
+        showPortfolioImg(PortfolioImgSource, PortfolioImgNow);
+    } 
+})
+
+// global
+window.addEventListener('load', () => {
+    // load all portfolio
+    loadAllPortfolio();
+})
